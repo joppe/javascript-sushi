00096d8bd3a113b427c2b364d454954011abda99
Robin Aldenhoven <robin@up-front.nl>
2017-12-01 16:47:22 +0100

    WIP

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 var app   = require('express')();
 var http  = require('http').Server(app);
-var io  = require('socket.io')(http);
+var io  = require('socket.io')(http, {pingInterval: 1000, pingTimeout: 2000});
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
@@ -11,13 +11,15 @@ io.on('connection', function(socket){
   io.emit('clients', Object.keys(io.sockets.clients().sockets));
 
   socket.on('disconnect', function(){
-    console.log('user disconnected');
-
     // Send connected clients to everyone else
     io.emit('clients', Object.keys(io.sockets.clients().sockets));
   });
 
-  socket.on('play', function(id){
-    socket.to(id).emit('play sound');
+  socket.on('play', function(clientId, callerId){
+    socket.broadcast.to(clientId).emit('playSound', callerId);
+  });
+
+  socket.on('latency', function(clientId, latency){
+    socket.broadcast.to(clientId).emit('remoteDelay', latency);
   });
 });
--- src/app/services/broadcaster.service.ts
+++ src/app/services/broadcaster.service.ts
@@ -10,6 +10,8 @@ export class BroadcasterService {
   private socket: ioClient;
   private connected = false;
   private plays: Number = 0;
+  private latency: Number;
+  private id: String;
 
   private clientsSubject: Subject<String[]> = new Subject();
   public clients: Observable<String[]> = this.clientsSubject.asObservable();
@@ -17,17 +19,21 @@ export class BroadcasterService {
   private playSubject: Subject<Number> = new Subject();
   public play: Observable<Number> = this.playSubject.asObservable();
 
+  private remoteLatencySubject: Subject<Number> = new Subject();
+  private remoteLatency: Observable<Number> = this.remoteLatencySubject.asObservable();
+
   constructor() {
     this.socket = new ioClient('http://raldenhoven.nl:3000');
 
-    this.socket.on('playSound', function(value) {
+    this.socket.on('playSound', function(remoteClient) {
       this.plays = this.plays + 1;
       this.playSubject.next(this.plays);
+      this.socket.emit('latency', remoteClient, this.latency);
     }.bind(this));
 
     this.socket.on('connect', function(){
       this.connected = true;
-      console.log('MY ID =', this.socket.id);
+      this.id = this.socket.id;
     }.bind(this));
 
     this.socket.on('clients', function(data){
@@ -35,11 +41,21 @@ export class BroadcasterService {
       this.clientsSubject.next(clients);
     }.bind(this));
 
+    this.socket.on('pong', function(delay){
+        this.latency = delay;
+    }.bind(this));
+
+    this.socket.on('remoteDelay', function(delay){
+        if (this.latency && delay) {
+            console.log(this.latency + delay);
+            this.remoteLatencySubject.next( this.latency + delay);
+        }
+    }.bind(this));
   }
 
   public playSound(clientId): void {
     if (this.connected) {
-      this.socket.emit('play', clientId);
+      this.socket.emit('play', clientId, this.id);
     }
   }
 }
