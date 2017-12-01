7e71b7069eeb6be11537b0942e2e440080ee8f11
Robin Aldenhoven <robin@up-front.nl>
2017-12-01 14:42:51 +0100

    Fixed broadcast / emit

--- server/index.js
+++ server/index.js
@@ -8,11 +8,15 @@ http.listen(3000, function(){
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  // Send connected clients to sender
+  socket.emit('clients', Object.keys(io.sockets.clients().sockets));
+  // Send connected clients to everyone else
   socket.broadcast.emit('clients', Object.keys(io.sockets.clients().sockets));
 
   socket.on('disconnect', function(){
     console.log('user disconnected');
 
+    // Send connected clients to everyone else
     socket.broadcast.emit('clients', Object.keys(io.sockets.clients().sockets));
   });
 
