1b3d6220c18a3d937d9669f954e2e41a844d4014
Robin Aldenhoven <robin@up-front.nl>
2017-12-01 14:06:03 +0100

    Updated server

--- server/index.js
+++ server/index.js
@@ -6,22 +6,18 @@ http.listen(3000, function(){
   console.log('listening on *:3000');
 });
 
-
 io.on('connection', function(socket){
   console.log('a user connected');
 
-  socket.on('join', function(room) {
-    console.log('a user joined room: ', room);
-    socket.join(room);
-  });
+  socket.emit('clients', io.sockets.clients());
 
   socket.on('disconnect', function(){
     console.log('user disconnected');
+
+    socket.emit('clients', io.sockets.clients());
   });
 
   socket.on('play sound at', function(id){
-    console.log('play sound at', id);
-    socket.broadcast.to(id).emit('play sound');
+    socket.to(id).emit('play sound');
   });
-
 });
